1ee0ad815e37b6103741dabb891e74b83b66acd3
Afranio Solano <argus@Mac-mini-de-Afranio.local>
2025-08-08 12:13:17 -0500

    refactor: rename namespace to 'mundose' and simplify metadata references

--- 4-monitoring_grafana.tf
+++ 4-monitoring_grafana.tf
@@ -1,13 +1,22 @@
 # 4-monitoring_grafana.tf
 
+# Crear el namespace explícitamente
+resource "kubernetes_namespace" "monitoring" {
+  metadata {
+    name = "monitoring"
+  }
+}
+
 resource "helm_release" "grafana" {
   name             = "grafana"
   namespace        = "monitoring"
   repository       = "https://grafana.github.io/helm-charts"
   chart            = "grafana"
-  create_namespace = true
+  create_namespace = false
   force_update     = true
   recreate_pods    = true
+
+  depends_on = [kubernetes_namespace.monitoring]
 
   values = [<<EOF
 adminPassword: "admin123"
@@ -62,6 +71,8 @@ resource "kubernetes_config_map" "grafana_dashboard" {
       grafana_dashboard = "1"
     }
   }
+
+  depends_on = [kubernetes_namespace.monitoring]
 
   data = {
     "node-exporter-full.json" = file("${path.module}/dashboards/1860-node-exporter-full.json")
